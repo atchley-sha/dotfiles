c584f292862086ee3dc5699f1bcfabea1191508e
Hayden Atchley <atchley.sha@gmail.com>
2022-05-16 11:19:54 -0600

    add script to link files on nix systems

---
+++ add_nix_links.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Adds symlinks for all *nix-relevant files in /dotfiles/
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/vimrc ~/.vimrc
