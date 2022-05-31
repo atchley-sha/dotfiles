ef36bb651981a87adc9b00bd19a8a47452aa3289
Hayden Atchley <atchley.sha@gmail.com>
2022-05-31 17:45:16 -0600

    add tmux config

--- sh/add_nix_links.sh
+++ sh/add_nix_links.sh
@@ -6,5 +6,5 @@ ln -sf ~/dotfiles/bash/bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
-ln -sf ~/dotfiles/tmux/tmux_config ~/.tmux.config
+ln -sf ~/dotfiles/tmux/tmux_conf ~/.tmux.conf
 ln -sf ~/dotfiles/R/Rprofile ~/.Rprofile
